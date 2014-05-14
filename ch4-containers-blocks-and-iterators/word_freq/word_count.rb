eabb6b7956ff636878544ff1f551fdc5eadacd3c
zhaqiang <hugochougt@gmail.com>
2014-05-14 22:52:04 +0800

    Refactor: use map method for iteration instead of for loop structure.

--- ch4-containers-blocks-and-iterators/word_freq/word_count.rb
+++ ch4-containers-blocks-and-iterators/word_freq/word_count.rb
@@ -11,8 +11,4 @@ counts = count_frequency(word_list)
 sorted = counts.sort_by { |word, count| count }
 top_five = sorted.last(5)
 
-for i in 0...5
-  word = top_five[i][0]
-  count = top_five[i][1]
-  puts "#{word}: #{count}"
-end
+puts top_five.map { |word, count| "#{word}: #{count}" }
