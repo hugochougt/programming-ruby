555428c52c0260a962d42addb95c971b28d16ec2
zhaqiang <hugochougt@gmail.com>
2014-05-19 22:15:45 +0800

    Add example code of minxins

---
+++ ch5-sharing-functionality-inheritance-modules-and-mixins/mixin_example.rb
@@ -0,0 +1,31 @@
+
+module Debug
+
+  def who_am_i?
+    "#{self.class.name} (id: #{self.object_id}): #{self.name}"
+  end
+end
+
+class Phonograph
+  include Debug
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class EightTrack
+  include Debug
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+ph = Phonograph.new("West End Blues")
+et = EightTrack.new("Surrealistic Pillow")
+
+puts ph.who_am_i?
+puts et.who_am_i?
