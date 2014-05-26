92d24fd335fc968cc885dfb3312dcba9a12d2053
zhaqiang <hugochougt@gmail.com>
2014-05-26 21:39:33 +0800

    Example of handling Exception

---
+++ ch10-exceptions-catch-and-throw/fetch_web_page.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+
+page = "podcasts"
+file_name = "#{page}.html"
+web_page = open("http://progprog.com/#{page}")
+output = File.open(file_name, "w")
+
+begin
+  while line = web_page.gets
+    output.puts line
+  end
+  output.close
+rescue Exception
+  STDERR.puts "Failed to download #{page}: #{$!}"
+  output.close
+  File.delete(file_name)
+  raise
+end
