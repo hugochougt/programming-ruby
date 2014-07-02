abdd98adfd07a13dac1ab5ed5bc0daf7ba8b39bd
zhaqiang <hugochougt@gmail.com>
2014-07-02 20:27:50 +0800

    A program that asks the user for his name and greets him with his name

---
+++ exercises/simple-programming-problems/elementary/p2_greet.rb
@@ -0,0 +1,5 @@
+# Write a program that asks the user for his name and greets him with his name
+
+print "Enter your name: "
+name = gets.chop            # gets with a tailing new line
+print "Hi, #{name}.\n"
