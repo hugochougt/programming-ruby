23f948b1f14a1f3a5a85a0e6018d564cfd21ae9d
zhaqiang <hugochougt@gmail.com>
2014-07-08 22:56:51 +0800

    A guessing game where the user has to guess a secret number

---
+++ exercises/simple-programming-problems/elementary/p9_guessing_secret_number.rb
@@ -0,0 +1,25 @@
+# Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether his number was too large or too small. At the end the number of tries needed should be printed. I counts only as one try if the user inputs the same number consecutively.
+
+guessing_time = 0
+prev_guessing = -1
+the_secret_number = Random.new.rand(9999)
+
+puts "Enter a number that greater than 0 (game will quit after enter a negative)" 
+
+while (num = gets.chop.to_i) > 0
+  next if num == prev_guessing
+  guessing_time += 1
+
+  if num > the_secret_number
+    puts "Too greate"
+  elsif num < the_secret_number
+    puts "Too small"
+  else
+    puts "Bingo!!"
+    break
+  end
+
+  prev_guessing = num
+end
+
+puts "You guessed #{guessing_time} times!"
