5c6c43635ba6b4632671316813412e1a75917d93
zhaqiang <hugochougt@gmail.com>
2014-07-14 21:27:21 +0800

    Add unit test for concatenate method

---
+++ exercises/simple-programming-problems/lists-strings/test_concatenate.rb
@@ -0,0 +1,14 @@
+require_relative 'p9_concatenate_two_lists'
+require 'test/unit'
+
+class TestConcatenate < Test::Unit::TestCase
+
+  def test_concatenate
+    lst1 = [123, 4]
+    lst2 = [567, 8, 9]
+
+    assert_equal([123, 4, 567, 8, 9], concatenate(lst1, lst2))
+  end
+
+end
+
