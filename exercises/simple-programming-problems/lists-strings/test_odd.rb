4742c2eacaf128fd9fb45a683eeea14dfdabc662
zhaqiang <hugochougt@gmail.com>
2014-07-14 20:40:50 +0800

    Add unit test for select_odd_items method

---
+++ exercises/simple-programming-problems/lists-strings/test_odd.rb
@@ -0,0 +1,21 @@
+require_relative 'p4_select_odd_items'
+require 'test/unit'
+
+class TestOdd < Test::Unit::TestCase
+
+  def test_select_odd_items
+    list = []
+    assert_equal([], select_odd_items(list))
+
+    list = [555]
+    assert_equal([555], select_odd_items(list))
+
+    list = [23, 46, 89, 1001]
+    assert_equal([23, 89], select_odd_items(list))
+
+    list = [99, 88, 77, 66, 55, 44, 33]
+    assert_equal([99, 77, 55, 33], select_odd_items(list))
+  end
+
+end
+
