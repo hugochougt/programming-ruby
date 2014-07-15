bab8bbb12a88f24e19dcc3ecf3593e7ace711520
zhaqiang <hugochougt@gmail.com>
2014-07-15 21:59:12 +0800

    A function that merges two sorted lists into a new list

---
+++ exercises/simple-programming-problems/lists-strings/p11_merge_sorted_lists.rb
@@ -0,0 +1,35 @@
+# Write a function that merges two sorted lists into a new list.
+
+def merge(sorted_lst1, sorted_lst2)
+  if sorted_lst1 == nil || sorted_lst1.length == 0
+    return sorted_lst2
+  end
+
+  if sorted_lst2 == nil || sorted_lst2.length == 0
+    return sorted_lst1
+  end
+
+  i = 0
+  j = 0
+  lst = []
+  while i < sorted_lst1.length && j < sorted_lst2.length
+    if sorted_lst1[i] < sorted_lst2[j]
+      lst << sorted_lst1[i]
+      i += 1
+    else
+      lst << sorted_lst2[j]
+      j += 1
+    end
+  end
+
+  if i < sorted_lst1.length
+    lst.concat sorted_lst1[i, sorted_lst1.length]
+  end
+
+  if j < sorted_lst2.length
+    lst.concat sorted_lst2[j, sorted_lst2.length]
+  end
+
+  return lst
+end
+
