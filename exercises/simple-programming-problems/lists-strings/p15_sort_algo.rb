25079bb9b1b63271e4bac3c680ccdd0585f27b2a
zhaqiang <hugochougt@gmail.com>
2014-07-22 16:36:59 +0800

    Add skeleton of solution to problem 15

---
+++ exercises/simple-programming-problems/lists-strings/p15_sort_algo.rb
@@ -0,0 +1,23 @@
+# Implement the following sorting algorithms: Selection sort, Insertion sort, Merge sort, Quick sort, Stooge Sort.
+
+def selection_sort!(arr)
+
+end
+
+def insertion_sort!(arr)
+
+end
+
+
+def merge_sort!(arr)
+
+end
+
+def quick_sort!(arr)
+
+end
+
+def stooge_sort!(arr)
+
+end
+
